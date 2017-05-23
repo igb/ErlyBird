25f4797065153eb5a2bfff3a8acc40b2a0239a85
ibrown <ibrown@twitter.com>
2017-05-22 17:00:41 -0700

    adding hacked version of escape_uri and a unit test stub

--- src/erlybird.erl
+++ src/erlybird.erl
@@ -1,5 +1,5 @@
 -module(erlybird).
--export([get_secrets/0, post/4,get_user_timeline/4, get_entire_timeline/4]).
+-export([get_secrets/0, post/4,get_user_timeline/4, get_entire_timeline/4,get_entire_timeline/5]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -26,28 +26,36 @@ get_user_timeline(Parameters, Consumer, AccessToken, AccessTokenSecret)->
     get_request(Url, Parameters, Consumer, AccessToken, AccessTokenSecret).
 
 get_entire_timeline(Parameters, Consumer, AccessToken, AccessTokenSecret)->
+    get_entire_timeline(Parameters, Consumer, AccessToken, AccessTokenSecret, 100000000).
 
-    get_entire_timeline(Parameters, Consumer, AccessToken, AccessTokenSecret, [], 0).
+get_entire_timeline(Parameters, Consumer, AccessToken, AccessTokenSecret, Limit)->
 
-get_entire_timeline(Parameters, Consumer, AccessToken, AccessTokenSecret,  Acc, LastTweetId)->
-   case LastTweetId of
-      0 -> NewParameters = Parameters;
-       _ -> NewParameters = lists:append(Parameters, [{max_id, integer_to_list(LastTweetId - 1)}])
-   end,
-    
+    get_entire_timeline(Parameters, Consumer, AccessToken, AccessTokenSecret, Limit, [], 0).
 
-	    
-    io:format("last tweet ID: ~p~n", [LastTweetId]),
-    Timeline = get_user_timeline(NewParameters, Consumer, AccessToken, AccessTokenSecret),
-    case Timeline of
-	[] ->
-	    Acc;
-	_ ->
-	    NewAcc=lists:append(Acc, Timeline),
-	    [{Tweet}|_]=lists:reverse(Timeline),
-	    {_, NewLastTweetId}=lists:keyfind(<<"id">>,1, Tweet),
-	    get_entire_timeline(Parameters, Consumer, AccessToken, AccessTokenSecret,  NewAcc, NewLastTweetId)
-    end.
+get_entire_timeline(Parameters, Consumer, AccessToken, AccessTokenSecret,  Limit, Acc, LastTweetId)->
+   case length(Acc) > Limit of
+       true ->
+	   Acc;
+       false ->
+	   case LastTweetId of
+	       0 -> NewParameters = Parameters;
+	       _ -> NewParameters = lists:append(Parameters, [{max_id, integer_to_list(LastTweetId - 1)}])
+	   end,
+	   
+	   
+	   
+	   io:format("last tweet ID: ~p~n", [LastTweetId]),
+	   Timeline = get_user_timeline(NewParameters, Consumer, AccessToken, AccessTokenSecret),
+	   case Timeline of
+	       [] ->
+		   Acc;
+	       _ ->
+		   NewAcc=lists:append(Acc, Timeline),
+		   [{Tweet}|_]=lists:reverse(Timeline),
+		   {_, NewLastTweetId}=lists:keyfind(<<"id">>,1, Tweet),
+		   get_entire_timeline(Parameters, Consumer, AccessToken, AccessTokenSecret, Limit,  NewAcc, NewLastTweetId)
+	   end
+   end.
     
  
 current_time_millis()->   
@@ -56,19 +64,29 @@ current_time_millis()->
 
 
 post(Tweet, Consumer, AccessToken, AccessTokenSecret)->
-    httpc:request(get,
-		  {"https://api.twitter.com/1.1/statuses/retweets/509457288717819904.json",
-		   [{"Accept", "*/*"},
+    Status="status=test",
+    io:format(Status),
+    BodyHash = base64:encode_to_string(crypto:hash(sha, Status)),
+    io:format(BodyHash),
+    Headers =  [{"Accept", "*/*"},
 		    {"Host","api.twitter.com"},
+		    {"Content-Type","application/x-www-form-urlencoded"},
 		    {"Authorization",
 		     lists:append("OAuth ",
 				  oauth:header_params_encode(
 
-oauth:sign("GET", "https://api.twitter.com/1.1/statuses/retweets/509457288717819904.json", [], Consumer, AccessToken, AccessTokenSecret)
+oauth:sign("POST", "https://api.twitter.com/1.1/statuses/update.json", [{"status", "test"}], Consumer, AccessToken, AccessTokenSecret)
 							    )
 				 )
 		    }
-		   ]
+		   ],
+    
+io:fwrite("~n~p~n", [Headers]),
+       httpc:request(post,
+		  {"http://api.twitter.com/1.1/statuses/update.json",
+		    Headers,
+                    "application/x-www-form-urlencoded",
+                    Status
 		  }, [], [{headers_as_is, true}]).
 
 
@@ -92,7 +110,53 @@ get_request(Url, Parameters, Consumer, AccessToken, AccessTokenSecret)->
     jiffy:decode(Body).
 
 
+escape_uri([C | Cs]) when C >= $a, C =< $z ->
+        [C | escape_uri(Cs)];
+escape_uri([C | Cs]) when C >= $A, C =< $Z ->
+	[C | escape_uri(Cs)];
+escape_uri([C | Cs]) when C >= $0, C =< $9 ->
+    [C | escape_uri(Cs)];
+escape_uri([C = $. | Cs]) ->
+    [C | escape_uri(Cs)];
+escape_uri([C = $- | Cs]) ->
+    [C | escape_uri(Cs)];
+escape_uri([C = $_ | Cs]) ->
+    [C | escape_uri(Cs)];
+escape_uri([C | Cs]) when C > 16#7f ->
+    %% This assumes that characters are at most 16 bits wide.
+    escape_byte(((C band 16#c0) bsr 6) + 16#c0)
+	++ escape_byte(C band 16#3f + 16#80)
+	++ escape_uri(Cs);
+escape_uri([C | Cs]) ->
+    escape_byte(C) ++ escape_uri(Cs);
+escape_uri([]) ->
+    [].
+
+escape_byte(C) when C >= 0, C =< 255 ->
+    [$%, hex_digit(C bsr 4), hex_digit(C band 15)].
+
+hex_digit(N) when N >= 0, N =< 9 ->
+    N + $0;
+hex_digit(N) when N > 9, N =< 15 ->
+    N + $A - 10.
+
 
 -ifdef(TEST).
-
+post_signature_test()->
+    Status="Hello Ladies + Gentlemen, a signed OAuth request!",
+    OauthConsumerKey="xvz1evFS4wEEPTGEFPHBog",
+    OauthNonce="kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg",
+    OauthSignatureMethod="HMAC-SHA1",
+    OauthTimestamp="1318622958",
+    OauthToken="370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb",
+    OauthVersion="1.0",
+    EscapedStatus = escape_uri(Status),
+    TestEncodedValue="Hello%20Ladies%20%2B%20Gentlemen%2C%20a%20signed%20OAuth%20request%21",
+    io:fwrite("~n"),
+    io:fwrite("~s~n", [EscapedStatus]),
+    io:fwrite("~s~n", [TestEncodedValue]),
+   % EscapedStatus="Hello%20Ladies%20%2B%20Gentlemen%2C%20a%20signed%20OAuth%20request%21",
+    ?assert(EscapedStatus  =:= TestEncodedValue),
+    ok.
+    
 -endif.
